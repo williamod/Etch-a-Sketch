36a25f8f043c3b651dbfd827e8f525dc3e99876e
williamod <williamodonnell016@gmail.com>
2024-03-07 18:30:41 -0800

    Add basic script and style for board creation

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+function alertFunction() {
+    alert('you did it');
+}
+function getSize() {
+    
+    while (true){
+        let boardSize = prompt('Choose size length:', 16);
+        
+
+        if (parseInt(boardSize) > 0 && parseInt(boardSize) <= 100) {
+            return boardSize
+        }
+        else {
+            alert('Please enter a number (Max:100)')
+        }
+    }
+}
+
+function makeRows(num) {
+    for (let i = 0; i < num; i++){
+        let row = document.createElement('div');
+        row.setAttribute('class', `row`);
+        row.setAttribute('id', `row${i}`);
+        board.appendChild(row)
+    }
+}
+
+function fillRows (num) {
+    let row = document.querySelectorAll('.row')
+    row.forEach((item) => {
+        console.log(item);
+        for (let i = 0; i < num; i++) {
+            let col = document.createElement('div');
+            col.setAttribute('class', 'col');
+            col.setAttribute('id', `col${i}`) 
+            item.appendChild(col)
+        }
+    })
+    
+}
+
+
+
+
+
+const board = document.querySelector('.board') 
+
+const reset = document.querySelector('#reset')
+let length;
+reset.onclick = () => {
+    length = getSize();
+    console.log(length);
+    makeRows(length);
+    fillRows(length);
+}
